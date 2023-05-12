89e2e32454086829fc8dc1858ecfe7f4d506c708
karmakatoro <karmakatoro@gmail.com>
2023-05-12 14:27:34 +0200

    customize 404 page

---
+++ resources/views/pages/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.base')
+
+@section('content')
+
+    <div class="main-wrapper error-wrapper">
+        <div class="error-box">
+            <h1>404</h1>
+            <h3><i class="fa fa-warning"></i> Oops! Page not found!</h3>
+            <p>The page you requested was not found.</p>
+            <a href="index-2.html" class="btn btn-primary go-home">Go to Home</a>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/forgot', [AuthController::class, 'forget'])->name('auth.forget');
 Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 
 // Dashboard routes
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
 
 
 
