5218dd35ce5b2d62dbd838d3ee26cedc98032881
karmakatoro <karmakatoro@gmail.com>
2023-05-12 12:48:32 +0200

    add login post routes and dash index

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,7 +22,11 @@ Route::get('/', function () {
 });
 
 // Auth routes
+Route::get('/login', [AuthController::class, 'index'])->name('auth.login');
+Route::post('/login', [AuthController::class, 'connect'])->name('auth.connect');
 
+// Dashboard routes
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 
 
