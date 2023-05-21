838e8d45f2925d7519633ead4fceeda4298db362
karmakatoro <karmakatoro@gmail.com>
2023-05-21 09:19:35 +0200

    settings
    add route view
    create view
    add company resource route

---
+++ resources/views/pages/settings.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.base')
+
+@section('Settings', 'Dashboard | '.env('APP_NAME'))
+
+@section('content')
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,6 +30,12 @@ Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 
 // Dashboard routes
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
+
+// Settings routes
+Route::get('/settings', function(){
+    return view('pages.settings');
+});
+Route::resource('/company', CompanyController::class);
 
 
 
