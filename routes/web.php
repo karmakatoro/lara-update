1597ae901ae8090de8a2f9cfdef7da03e2324335
karmakatoro <karmakatoro@gmail.com>
2023-05-21 12:02:57 +0200

    Update web.php
    create resource route for all params of settings page
    create also the get route for change user password

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,14 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\EmailSettingsController;
+use App\Http\Controllers\InvoiceSettingsController;
+use App\Http\Controllers\LeaveTypeController;
 use App\Http\Controllers\LocalizationController;
-use App\Models\Localization;
+use App\Http\Controllers\NotificationsSettingsController;
+use App\Http\Controllers\RolesPermissionController;
+use App\Http\Controllers\SalarySettingsController;
+use App\Http\Controllers\ThemeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -37,8 +43,18 @@ Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard'
 Route::get('/settings', function(){
     return view('pages.settings');
 })->name('settings');
-Route::resource('/company', CompanyController::class);
-Route::resource('/localization', LocalizationController::class);
+Route::resource('/company-settings', CompanyController::class);
+Route::resource('/localization-settings', LocalizationController::class);
+Route::resource('/theme-settings', ThemeController::class);
+Route::resource('/roles-permissions-settings', RolesPermissionController::class);
+Route::resource('/email-settings', EmailSettingsController::class);
+Route::resource('/invoice-settings', InvoiceSettingsController::class);
+Route::resource('/salary-settings', SalarySettingsController::class);
+Route::resource('/notifications-settings', NotificationsSettingsController::class);
+Route::resource('/leave-type-settings', LeaveTypeController::class);
+Route::get('/change-password-settings', [AuthController::class, 'show_change_password'])->name('change-password-settings');
+
+
 
 
 
