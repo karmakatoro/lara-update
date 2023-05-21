92af281516ba3913c618f782720f332ec7e03290
karmakatoro <karmakatoro@gmail.com>
2023-05-21 12:10:04 +0200

    Update navbar.blade.php
    add dashboard route name on logo in the navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <div class="header">
     <div class="header-left">
-        <a href="index-2.html" class="logo">
+        <a href="{{ route('dashboard') }}" class="logo">
             <img src="assets/img/logo.png" width="35" height="35" alt=""> <span>Preclinic</span>
         </a>
     </div>
