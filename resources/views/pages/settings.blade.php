4c3042961f141aef9a792b10b6f15213fc75f133
karmakatoro <karmakatoro@gmail.com>
2023-05-21 10:46:30 +0200

    Update settings.blade.php
    include components on condition

--- resources/views/pages/settings.blade.php
+++ resources/views/pages/settings.blade.php
@@ -3,12 +3,19 @@
 @section('title', 'Settings | '.env('APP_NAME'))
 
 @section('content')
-    <div class="main-wrapper">
-        {{-- Include navbar component --}}
-        @include('components.navbar')
-        {{-- Include second sidebar --}}
-        @include('components.second-sidebar')
-        {{-- Include main content --}}
+<div class="main-wrapper">
+    {{-- Include navbar component --}}
+    @include('components.navbar')
+    {{-- Include second sidebar --}}
+    @include('components.second-sidebar')
+    {{-- Include main content --}}
+    <div class="page-wrapper">
+        @if(request()->route()->getName()=='settings')
         @include('pages.settings.company')
+        @elseif (request()->route()->getName()=='localization.index')
+        @include('pages.settings.localization')
+        @endif
     </div>
+
+</div>
 @endsection
