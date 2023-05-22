8c013c2ec0667dfaeada5689a243e0abbc8e6121
karmakatoro <karmakatoro@gmail.com>
2023-05-22 20:45:52 +0200

    Update change-password.blade.php
    add sessions messages

--- resources/views/pages/settings/change-password.blade.php
+++ resources/views/pages/settings/change-password.blade.php
@@ -7,13 +7,26 @@
     <div class="row">
         <div class="col-md-6 offset-md-3">
             <h4 class="page-title">Change Password</h4>
+            @if(session()->has('error'))
+            <div class="alert alert-danger">
+                {{ session()->get('error') }}
+            </div>
+            @endif
+            @if(session()->has('success'))
+            <div class="alert alert-success">
+                {{ session()->get('success') }}
+            </div>
+            @endif
             <form method="POST" action="{{ route('change-password') }}">
                 @csrf
                 <div class="row">
                     <div class="col-sm-12">
                         <div class="form-group">
                             <label>Old password</label>
-                            <input type="password" class="form-control" name="old">
+                            <input type="password" class="form-control" name="old_password">
+                            @if($errors->has('old_password'))
+                            <span class="text-danger mt-2">{{ $errors->first('old_password') }}</span>
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -21,13 +34,19 @@
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label>New password</label>
-                            <input type="password" class="form-control" name="new">
+                            <input type="password" class="form-control" name="password">
+                            @if($errors->has('password'))
+                            <span class="text-danger mt-2">{{ $errors->first('password') }}</span>
+                            @endif
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label>Confirm password</label>
-                            <input type="password" class="form-control" name="confirm">
+                            <input type="password" class="form-control" name="password_confirmation">
+                            @if($errors->has('password'))
+                            <span class="text-danger mt-2">{{ $errors->first('password') }}</span>
+                            @endif
                         </div>
                     </div>
                 </div>
