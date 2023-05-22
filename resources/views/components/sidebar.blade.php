0a7ab9cdf364f018accbb09fb087e563495374c2
karmakatoro <karmakatoro@gmail.com>
2023-05-22 10:05:14 +0200

    Update sidebar.blade.php
    remove menu-title main

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -2,7 +2,7 @@
     <div class="sidebar-inner slimscroll">
         <div id="sidebar-menu" class="sidebar-menu">
             <ul>
-                <li class="menu-title">Main</li>
+                {{-- <li class="menu-title">Main</li> --}}
                 <li class="@if(request()->route()->getName()=='dashboard') active @endif">
                     <a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
                 </li>
