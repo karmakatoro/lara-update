cbefe6323ee8f1b301216360ad87cc7e40818ea3
karmakatoro <karmakatoro@gmail.com>
2023-05-22 10:27:18 +0200

    Update change-password.blade.php
    add route on action

--- resources/views/pages/settings/change-password.blade.php
+++ resources/views/pages/settings/change-password.blade.php
@@ -7,12 +7,13 @@
     <div class="row">
         <div class="col-md-6 offset-md-3">
             <h4 class="page-title">Change Password</h4>
-            <form>
+            <form method="POST" action="{{ route('change-password') }}">
+                @csrf
                 <div class="row">
                     <div class="col-sm-12">
                         <div class="form-group">
                             <label>Old password</label>
-                            <input type="password" class="form-control">
+                            <input type="password" class="form-control" name="old">
                         </div>
                     </div>
                 </div>
@@ -20,19 +21,19 @@
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label>New password</label>
-                            <input type="password" class="form-control">
+                            <input type="password" class="form-control" name="new">
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label>Confirm password</label>
-                            <input type="password" class="form-control">
+                            <input type="password" class="form-control" name="confirm">
                         </div>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-sm-12 text-center m-t-20">
-                        <button type="button" class="btn btn-primary submit-btn">Update Password</button>
+                        <button type="sumbit" class="btn btn-primary submit-btn">Update Password</button>
                     </div>
                 </div>
             </form>
