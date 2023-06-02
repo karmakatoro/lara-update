00c416c26ad9ef9dddb983f4b99200397f411d1c
karmakatoro <karmakatoro@gmail.com>
2023-06-02 20:57:05 +0200

    Update leave-type.blade.php

--- resources/views/pages/settings/leave-type.blade.php
+++ resources/views/pages/settings/leave-type.blade.php
@@ -14,50 +14,8 @@
     </div>
     <div class="row">
         <div class="col-md-12">
-            <div class="table-responsive">
-                <table class="table table-striped custom-table">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Leave Type</th>
-                            <th>Leave Days</th>
-                            <th>Status</th>
-                            <th class="text-right">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($leaveTypes as $leaveType )
-                        <tr>
-                            <td>
-                                {{ $leaveType->id }}
-                            </td>
-                            <td>{{ $leaveType->name }}</td>
-                            <td>{{ $leaveType->days }}</td>
-                            <td>
-                                <div class="dropdown action-label">
-                                    <a class="custom-badge status-green dropdown-toggle" href="#" data-toggle="dropdown" aria-expanded="false">
-                                        {{ Str::ucfirst($leaveType->status)  }}
-                                    </a>
-                                    <div class="dropdown-menu dropdown-menu-right">
-                                        <a href="#" class="dropdown-item">Active</a>
-                                        <a href="#" class="dropdown-item">Inactive</a>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="text-right">
-                                <div class="dropdown dropdown-action">
-                                    <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
-                                    <div class="dropdown-menu dropdown-menu-right">
-                                        <a class="dropdown-item" href="{{ route('leave-type-settings.edit', $leaveType) }}"><i class="fa fa-pencil m-r-5"></i> Edit</a>
-                                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_leavetype"><i class="fa fa-trash-o m-r-5"></i> Delete</a>
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
-                        @endforeach
-
-                    </tbody>
-                </table>
+            <div class="table-responsive" id="show_all">
+                <h1 class="text-center text-secondary my-5">Loading...</h1>
             </div>
         </div>
     </div>
@@ -99,4 +57,24 @@
         </div>
     </div>
 </div>
+<script>
+    $(document).ready(function ($) {
+    // Show all leave types
+    fetchAllEmployees();
+
+    function fetchAllEmployees() {
+        var url = "{{ route('leave-type.fetch')}}";
+        $.ajax({
+            url: url,
+            method: 'get',
+            success: function (response) {
+                $("#show_all").html(response);
+                $("table").DataTable({
+                    order: [0, 'desc']
+                });
+            }
+        });
+    }
+});
+</script>
 @endsection
